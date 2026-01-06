c1967343219e22082b0154083a3aba4008103911
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-01-06 14:24:58 +0700

    sua_reset_password

--- src/services/resetPassword.services.ts
+++ src/services/resetPassword.services.ts
@@ -1,3 +1,5 @@
+//Copy cai file nay qua gile user.services, sau do cap nhat lai duogn dan den file nay trong file user.controller.ts Con lai thi tuy may dua 
+
 import prisma from "~/configs/mysqlPrisma.config";
 import { generateHash } from "~/utlis/hash";
 import HttpStatus from "~/utlis/statusMap";
