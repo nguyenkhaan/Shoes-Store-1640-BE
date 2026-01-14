53c2af46c483f579c8d60539d234ec766bbb43d8
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-01-14 13:42:10 +0700

    add_cors

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import path from 'node:path'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const appConfig = (app: express.Express) => 
 {
     //Receiving data 
+    app.use(
+        cors({
+          origin: true,         
+          credentials: true,   
+        })
+      );
     app.use(express.json())    //json 
     app.use(express.urlencoded({extended : true})) //form - data 
     app.use(express.static('~/templates'))
