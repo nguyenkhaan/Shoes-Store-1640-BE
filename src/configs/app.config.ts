c5e104ed09f8292167f80b592bcf3503aa6d33b1
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-01-14 13:33:36 +0700

    add_cookies_parser

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import path from 'node:path'
+import cookieParser from 'cookie-parser'
 const appConfig = (app: express.Express) => 
 {
     //Receiving data 
     app.use(express.json())    //json 
     app.use(express.urlencoded({extended : true})) //form - data 
     app.use(express.static('~/templates'))
+    app.use(cookieParser())
 }
 
 export default appConfig
